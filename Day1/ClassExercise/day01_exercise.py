b94e6998060bca6087212367cf24a9ed0fe64573
Alex Avery <a.d.avery@wustl.edu>
2022-08-09 10:04:22 -0500

    updates

--- Day1/ClassExercise/day01_exercise.py
+++ Day1/ClassExercise/day01_exercise.py
@@ -5,24 +5,72 @@
 # Write a for loop, while loop, or function (or all three!) to create a
 # list of the first 10 numbers of the fibonacci sequence
 
+def fibonacci(x):
+    sequence = []
+    if x == 1:
+        sequence = [0]
+    else: 
+        sequence = [0,1]
+        for i in range(1, x-1):
+            sequence.append(sequence[i-1] + sequence[i])
+    return(sequence)
 
+fibonacci(11)
+    
 
 """return true if there is no e in 'word', else false"""
 def has_no_e(word):
+    if 'e' not in word:
+        return True
+    else:
+        return False 
 
-
+has_no_e('dog')
+has_no_e('bear')
+    
 """return true if there is e in 'word', else false"""
 def has_e(word):
+    if 'e' in word:
+        return True
+    else:
+        return False
+
+has_e('dog')
+has_e('bear')
 
 
 """return true if word1 contains only letters from word2, else false"""
 def uses_only(word1, word2):
+    return all([letter in word2 for letter in word1])
+
+uses_only('centour', 'cent')
+    
 
 
 """return true if word1 uses all the letters in word2, else false"""
 def uses_all(word1, word2):
+    return all([letter in word1 for letter in word2])
+
+uses_all('centour', 'cent')
+
 
 
 """true/false is the word in alphabetical order?"""
 # Hint: check the methods for lists
+#create empty list to check methods
+empty = []
+dir(empty)
+type(empty)
+
 def is_abecedarian(word):
+    list1 = [letter for letter in word]
+    list2 = [letter for letter in word]
+    list2.sort()
+    return list1 == list2
+
+is_abecedarian('abcdef')
+is_abecedarian('abcedf')
+    
+
+
+
--- Day1/Lab/lab01.py
+++ Day1/Lab/lab01.py
@@ -7,36 +7,82 @@
 
 def binarify(num): 
   """convert positive integer to base 2"""
-  if num<=0: return '0'
   digits = []
-  return ''.join(digits)
+  if num <= 0: 
+      return '0'
+  else:
+      while num > 0 :
+          digits.append(str(num % 2))
+          num = num // 2
+  return ''.join(digits[::-1])
+
+#test
+binarify(29)
 
 def int_to_base(num, base):
   """convert positive integer to a string in any base"""
-  if num<=0:  return '0' 
   digits = []
-  return ''.join(digits)
+  if num <= 0:  
+      return '0' 
+  else: 
+      while num > 0:
+          digits.append(str(num % base))
+          num = num // base 
+  return ''.join(digits[::-1])
+
+#test
+int_to_base(29, 2)
 
 def base_to_int(string, base):
-  """take a string-formatted number and its base and return the base-10 integer"""
-  if string=="0" or base <= 0 : return 0 
-  result = 0 
+  """take a string-formatted number and its base and return the base-10 integer"""  
+  if string == '0' or base <= 0 : return 0 
+  result = sum([int(x)*(base**i) for i, x in zip(reversed(range(0,len(string))), string)])
   return result 
+
+#test
+base_to_int('11101', 2)
 
 def flexibase_add(str1, str2, base1, base2):
   """add two numbers of different bases and return the sum"""
-  result = int_to_base(tmp, base1)
+  if str1 == '0' or base1 <= 0 : return 0 
+  result1 = sum([int(x)*(base1**i) for i, x in zip(reversed(range(0,len(str1))), str1)])
+  if str2 == '0' or base2 <= 0 : return 0 
+  result2 = sum([int(x)*(base2**i) for i, x in zip(reversed(range(0,len(str2))), str2)])
+  result = result1 + result2
   return result 
+
+#test
+flexibase_add('11101', '11101', 2, 2)
 
 def flexibase_multiply(str1, str2, base1, base2):
   """multiply two numbers of different bases and return the product"""
-  result = int_to_base(tmp, base1)
+  if str1 == '0' or base1 <= 0 : return 0 
+  result1 = sum([int(x)*(base1**i) for i, x in zip(reversed(range(0,len(str1))), str1)])
+  if str2 == '0' or base2 <= 0 : return 0 
+  result2 = sum([int(x)*(base2**i) for i, x in zip(reversed(range(0,len(str2))), str2)])
+  result = result1 * result2
   return result 
+
+#test
+flexibase_multiply('11101', '11101', 2, 2)
 
 def romanify(num):
   """given an integer, return the Roman numeral version"""
+  roman_numerals = [
+      (1000, 'M'),
+      (900, 'CM'),
+      (500, 'D'),
+      (400, 'CD'),
+      (100, 'C'),
+      (90, ''),
+      (50, ),
+      (40, ),
+      (10, ''),
+      (5, ''),
+      (1, ''),
+      ]
   result = ""
-  return result
+  return resuls
 
 
 # Copyright (c) 2014 Matt Dickenson
