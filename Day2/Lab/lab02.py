5c036b1a844f38ee82c6861d32af317a9388cd33
Alex Avery <a.d.avery@wustl.edu>
2022-08-09 16:00:04 -0500

    Update lab02.py

--- Day2/Lab/lab02.py
+++ Day2/Lab/lab02.py
@@ -7,21 +7,50 @@ class Clock:
 
     ## Print the time
     def __str__(self):
+        if len(str(self.minutes)) == 1: 
+            return (f"{self.hour}:0{self.minutes}")
+        else: 
+            return  (f"{self.hour}:{self.minutes}")
     
     ## Add time
     ## Don't return anything
     def __add__(self,minutes):
-    
+        #first we need to convert the time to minutes 
+        time_minutes = ((self.hour * 60) + self.minutes) 
+        #now we can add time 
+        added_time = time_minutes + minutes
+        #next we need to account for if adding time goes over a day 
+        added_time = added_time % (24 * 60)
+        #now we need to convert this back to the original format 
+        self.hour = added_time // 60
+        self.minutes = added_time % 60 
+        
     ## Subtract time
     ## Don't return anything
     def __sub__(self,minutes):
-    
+        #first we need to convert the time to minutes 
+        time_minutes = ((self.hour * 60) + self.minutes) 
+        #now we can subtract time 
+        added_time = time_minutes - minutes
+        #next we need to account for if adding time goes over a day 
+        added_time = added_time % (24 * 60)
+        #now we need to convert this back to the original format 
+        self.hour = added_time // 60
+        self.minutes = added_time % 60 
+
     ## Are two times equal?
     def __eq__(self, other):
+        if self.hour == other.hour and self.minutes == other.minutes:
+            return True
+        else:
+            return False
     
     ## Are two times not equal?
     def __ne__(self, other):
-
+        if self.hour != other.hour or self.minutes != other.minutes:
+            return True
+        else:
+            return False
 
 # You should be able to run these
 clock1 = Clock(23, 5)
