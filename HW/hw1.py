dc498ce95ea5576a8dafa2a788f6d0849806bee8
Alex Avery <a.d.avery@wustl.edu>
2022-08-14 13:51:44 -0500

    define cash function

--- HW/hw1.py
+++ HW/hw1.py
@@ -4,8 +4,25 @@
 
 # create portfolio class
 class Portfolio:
+    # create initializer for portfolio class
     def _int_(self):
         self.cash = []
+    # create function to add or remove cash
+    def Cash(self, amount, transaction_type):
+        # only allow positive amounts of cash
+        if amount <= 0:
+            print("Please specify an amount greater than zero to deposit or withdraw")
+        # for deposits:
+        elif transaction_type == 'deposit':
+            self.cash.append(amount)
+        # for withdraws: 
+        elif transaaction_type == 'withdraw':
+            self.cash.append(-1 * amount)
+        # for anything not a deposit or withdraw
+        else:
+            print("Please specify transaction type as deposit or withdraw")
+            
+        
         
 
     
@@ -16,3 +33,5 @@ class Stock:
 # create Mutual Funds class
 class Mutual_Funds:
     
+
+
