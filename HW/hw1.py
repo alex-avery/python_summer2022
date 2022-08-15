5e3b1d9527226db4f121c1f3003f0c12fb411814
Alex Avery <a.d.avery@wustl.edu>
2022-08-14 23:09:39 -0500

    working buyStocks function

--- HW/hw1.py
+++ HW/hw1.py
@@ -6,9 +6,11 @@
 class Portfolio:
     
     # create initializer for portfolio class
-    def __init__(self, cash = 0):
+    def __init__(self, cash = 0, stocks = {}):
         # amount of cash in portfolio
         self.cash = cash
+        # stocks and shares in portfolio 
+        self.stocks = stocks
           
     # create function to add cash
     def addCash(self, amount):
@@ -27,10 +29,18 @@ class Portfolio:
         # remove cash amount 
         else:
             self.cash -= amount
-      
-        
-        
-
+    
+    #create function to buy stocks 
+    def buyStock(self, shares, stock):
+        # can only buy stocks worth the amount of cash in portfolio
+        stock_total = stock.price * shares
+        if stock_total > self.cash:
+            print("You do not have enough cash to cover this transaction.")
+        else:
+            # add stock shares to portfolio 
+            self.stocks[stock.symbol] = {"shares" : shares}
+            # subtract stock price from cash
+            self.cash -= stock_total
     
 # create Stock class 
 class Stock:
@@ -45,18 +55,18 @@ class Stock:
 # create Mutual Funds class
 class MutualFund:
     # create initializer for mutual fund class
-    def __init__(self, ):
+    def __init__(self, symbol):
         # mutual funds symbol
         self.symbol = symbol
     
 
 # REQUIREMENTS:
-portfolio = Portfolio() #Creates a new portfolio
-portfolio.addCash(300.50) #Adds cash to the portfolio
-s = Stock(20, "HFH") #Create Stock with price 20 and symbol "HFH"
-portfolio.buyStock(5, s) #Buys 5 shares of stock s
-mf1 = MutualFund("BRT") #Create MF with symbol "BRT"
-mf2 = MutualFund("GHT") #Create MF with symbol "GHT"
+portfolio = Portfolio() #Creates a new portfolio (check)
+portfolio.addCash(300.50) #Adds cash to the portfolio (check)
+s = Stock(20, "HFH") #Create Stock with price 20 and symbol "HFH" (check)
+portfolio.buyStock(5, s) #Buys 5 shares of stock s (check)
+mf1 = MutualFund("BRT") #Create MF with symbol "BRT" (check)
+mf2 = MutualFund("GHT") #Create MF with symbol "GHT" (check)
 portfolio.buyMutualFund(10.3, mf1) #Buys 10.3 shares of "BRT"
 portfolio.buyMutualFund(2, mf2) #Buys 2 shares of "GHT"
 print(portfolio) #Prints portfolio
