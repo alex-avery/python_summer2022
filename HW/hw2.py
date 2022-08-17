4489b55b953508a006430b14b2cb4e7702af56bc
Alex Avery <a.d.avery@wustl.edu>
2022-08-16 23:01:01 -0500

    some updates

--- HW/hw2.py
+++ HW/hw2.py
@@ -20,21 +20,30 @@ import csv
 with open('biden_speeches.csv', 'w') as f:
     w = csv.DictWriter(f, fieldnames = ("Title", "Date", "Full Text", "Citation/Footnote"))
     w.writeheader()
-    web_address =  'https://www.presidency.ucsb.edu/documents/app-categories/presidential/spoken-addresses-and-remarks'
+    web_url =  'https://www.presidency.ucsb.edu/documents/app-categories/presidential/spoken-addresses-and-remarks'
     # first we need to loop through all the pages 
     # there are 253 pages
     counter = 0
     for i in range(107):
         counter += 1
         print("Working on page " + str(counter) + " url")
+        address_url = []
         if i == 0: 
             address_url = web_address    
         else: 
-            address_url = web_address + "?page=" + str(i)
-            address_page = urllib.request.urlopen(address_url) # read the web address of each page with urllib
-            soup = BeautifulSoup(address_page.read()) # open each page with soup
-            address_title = soup.findAll('div', {'class' : 'field-title'})
-            
+            address_url.append() = web_address + "?page=" + str(i)
+        address_page = urllib.request.urlopen(address_url) # read the web address of each page with urllib
+        soup = BeautifulSoup(address_page.read()) # open each page with soup
+    for i in range(0, 60): 
+        address = {}
+        address_title = soup.findAll('div', {'class' : 'field-title'})
+        
+        
+
+        
+
+       
+         
 
         
         
