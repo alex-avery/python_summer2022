b853dc859f16a1d50c1ddddfb7e5ad7c5fc1498b
Alex Avery <a.d.avery@wustl.edu>
2022-08-17 01:08:26 -0500

    add in helpful comments

--- HW/hw2.py
+++ HW/hw2.py
@@ -16,18 +16,26 @@
 from bs4 import BeautifulSoup
 import urllib.request
 import csv 
+import time
+import random
+import os
+
+# directory to store file 
+os.chdir('/Users/alexcisco/Library/Mobile Documents/com~apple~CloudDocs/Documents/WUSTL/Courses/Python Course/python_summer2022/HW')
 
 with open('biden_speeches.csv', 'w') as f:
     w = csv.DictWriter(f, fieldnames = ("Title", "Date", "Full Text", "Citation/Footnote"))
     w.writeheader()
+    # given web address for assignment
     web_address =  'https://www.presidency.ucsb.edu/documents/app-categories/presidential/spoken-addresses-and-remarks'
+    # using 60 items per page to avoid having to loop over the web adress more times
     web_address_60 = web_address + '?items_per_page=60'
-    # first we need to loop through all the pages 
-    # there are 253 pages
+    # adding a counter to tell us what page it is currently working on 
     counter = 0  
-    for i in range(253):
+    # Biden's inagural address is on page 11
+    for i in range(11):
         counter += 1
-        print("Working on page " + str(counter) + " url")
+        print("Working on page " + str(counter))
         if i == 0: 
             page_url = web_address_60    
         else: 
@@ -36,24 +44,31 @@ with open('biden_speeches.csv', 'w') as f:
         page_soup = BeautifulSoup(page.read()) 
         address_title = page_soup.find_all('div', {'class' : 'field-title'})
         for i in address_title: 
+            # get the unique url for each address
             address_url = 'https://www.presidency.ucsb.edu' + i.find('a')['href']
             address_page = urllib.request.urlopen(address_url)
             address_soup = BeautifulSoup(address_page.read())
+            # create empty dictionary 
             address = {}
+            #We only want Biden's speeches
             if address_soup.find('h3', {'class' : 'diet-title'}).text == 'Joseph R. Biden': 
                 try:
-                    address["Title"] = address_soup.find('div', {'class' : 'field-ds-doc-title'})
-                    address["Date"] = address_soup.find('span', {'class' : 'date-display-single'})
-                    address["Full Text"] = address_soup.find('div', {'class' : 'field-docs-content'})
-                    address["Citation/Footnote"] = address_soup.find('div', {'class' : 'field-docs-footnote'})
+                    address["Title"] = address_soup.find('div', {'class' : 'field-ds-doc-title'}).text
+                    address["Date"] = address_soup.find('span', {'class' : 'date-display-single'}).text
+                    address["Full Text"] = address_soup.find('div', {'class' : 'field-docs-content'}).text
+                    address["Citation/Footnote"] = address_soup.find('div', {'class' : 'field-docs-footnote'}).text
+                # some addresses do not have footnotes
                 except:
                     address["Citation/Footnote"] = "NA"
+                w.writerow(address)
+                # add sleep time so we do not accidentaly break the site 
+                time.sleep(random.uniform(1,5))
+            # skip if not an address given by Biden     
             else: 
                 break
                 
                 
      
-
 
 
        
