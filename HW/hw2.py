38e8fd9aeb667f738f5a567d8529f1a1516b1e13
Alex Avery <a.d.avery@wustl.edu>
2022-08-16 19:51:16 -0500

    add assignment requirements & import modules

--- HW/hw2.py
+++ HW/hw2.py
@@ -2,5 +2,20 @@
 # Homework 2
 # Alex Avery
 
+# Assignment reequirements: 
+
+# Go to: https://www.presidency.ucsb.edu/documents/app-categories/presidential/spoken-addressesand-remarks
+# Create a csv file with the following information for each spoken address given by President Biden since he became president on 2021-01-20:
+    # Date of spoken address
+    # Title
+    # Full text of address or remarks
+    # Citation/footnote (if one exists)
+# Remember, be polite and sleep after accessing each individual document page!
+
+# import necessary modules
+from bs4 import BeautifulSoup
+import urllib.request
+import csv 
 
 
+    
