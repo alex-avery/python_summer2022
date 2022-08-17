ab823891ae0e853a6271df822b3dd55278019e13
Alex Avery <a.d.avery@wustl.edu>
2022-08-17 00:13:31 -0500

    find title for each address

--- HW/hw2.py
+++ HW/hw2.py
@@ -20,26 +20,29 @@ import csv
 with open('biden_speeches.csv', 'w') as f:
     w = csv.DictWriter(f, fieldnames = ("Title", "Date", "Full Text", "Citation/Footnote"))
     w.writeheader()
-    web_url =  'https://www.presidency.ucsb.edu/documents/app-categories/presidential/spoken-addresses-and-remarks'
+    web_address =  'https://www.presidency.ucsb.edu/documents/app-categories/presidential/spoken-addresses-and-remarks'
+    web_address_60 = web_address + '?items_per_page=60'
     # first we need to loop through all the pages 
     # there are 253 pages
-    counter = 0
-    for i in range(107):
+    counter = 0  
+    for i in range(253):
         counter += 1
         print("Working on page " + str(counter) + " url")
-        address_url = []
         if i == 0: 
-            address_url = web_address    
+            page_url = web_address_60    
         else: 
-            address_url.append() = web_address + "?page=" + str(i)
-        address_page = urllib.request.urlopen(address_url) # read the web address of each page with urllib
-        soup = BeautifulSoup(address_page.read()) # open each page with soup
-    for i in range(0, 60): 
-        address = {}
-        address_title = soup.findAll('div', {'class' : 'field-title'})
-        
-        
-
+            page_url = web_address_60 + "?page=" + str(i)
+        page = urllib.request.urlopen(page_url) 
+        page_soup = BeautifulSoup(page.read()) 
+        address_title = soup.find_all('div', {'class' : 'field-title'})
+        for i in address_title: 
+            address_url = 'https://www.presidency.ucsb.edu' + i.find('a')['href']
+            address_page = urllib.request.urlopen(address_url)
+            address_soup = BeautifulSoup(address_page.read())
+            address = {}
+            if address_soup.find('h3', {'class' : 'diet-title'}).text == 'Joseph R. Biden': 
+                address["Title"] = address_soup.find('div', {'class' : 'field-ds-doc-title'})
+  
         
 
        
