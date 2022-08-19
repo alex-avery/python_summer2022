0ac44a4881d77ab60de899ca340f34c957c83818
Alex Avery <a.d.avery@wustl.edu>
2022-08-19 10:56:00 -0500

    import modules & set directory

--- HW/hw3.py
+++ HW/hw3.py
@@ -2,3 +2,10 @@
 # Homework 3
 # Alex Avery
 
+# import necessary modules
+import os
+import importlib 
+import tweepy
+
+# set directory to python course — homework file 
+os.chdir('/Users/alexcisco/Library/Mobile Documents/com~apple~CloudDocs/Documents/WUSTL/Courses/Python Course/python_summer2022/HW')
