87f10855df09287575c9511e211b75e2fcbc7fe5
Alex Avery <a.d.avery@wustl.edu>
2022-08-26 22:49:02 -0500

    create node class

--- HW/hw5.py
+++ HW/hw5.py
@@ -1,3 +1,11 @@
 # Python Course — Summer 2022
 # Homework 5
 # Alex Avery
+
+# first create node class (given in assignment instructions)
+class Node: 
+    def __init__(self, _value=None, _next=None):
+        self.value = _value
+        self.next = _next 
+    def __str__(self):
+        return str(self.value)
