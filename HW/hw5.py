65e3dfc670c78a4b5cd140e8b945b8ef0e7e83e5
Alex Avery <a.d.avery@wustl.edu>
2022-08-26 23:10:12 -0500

    create linkedlist class

--- HW/hw5.py
+++ HW/hw5.py
@@ -4,8 +4,26 @@
 
 # first create node class (given in assignment instructions)
 class Node: 
+    
     def __init__(self, _value=None, _next=None):
         self.value = _value
         self.next = _next 
+        
     def __str__(self):
         return str(self.value)
+
+# next create linkedlist class
+class LinkedList:
+    
+    def _init_(self):
+        self.head = None
+        
+    def addNode(self,new_value):
+        new_node = Node(new_value)
+        if self.head == None:
+            self.head = new_node
+        else:
+            last_node = self.head
+            last_node = last_node.next 
+            last_node.next = new_node
+        
