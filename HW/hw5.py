b202acbb845f98bb116f7411fc7070c40bec7f18
Alex Avery <a.d.avery@wustl.edu>
2022-08-26 23:49:16 -0500

    create removeNode function

--- HW/hw5.py
+++ HW/hw5.py
@@ -29,7 +29,47 @@ class LinkedList:
     
     def addNodeAfter(self, new_value, after_node):
         new_node = Node(new_value)
+        if after_node == None:
+            print("The previous node does not exist.")
+            return
         new_node.next = after_node.next
         after_node.next = new_node
- 
+    
+    def addNodeBefore(self, new_value, before_node):
+        new_node = Node(new_value)
+        if before_node == None:
+            print("The node does not exist.")
+            return
+        position = self.linkedlist.index(before_node.value)
+        self.linkedlist.insert(position, new_value.value)
+    
+    def removeNode(self, node_to_remove):
+        head = self.head 
+        if head != None:
+            if head.value == node_to_remove:
+                self.head = head.next 
+                return
+        while head != None:
+            if head.value == node_to_remove:
+                break
+            previous = head
+            head = head.next 
+        if head == None:
+            return
+        previous.next = head.next 
+        head = None
         
+        
+
+
+
+
+
+
+
+         
+
+         
+
+        
+
